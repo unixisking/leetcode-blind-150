5e3bdbdde76e3d8096e55ccff7a2474c6d5acb4a
Sid Ahmed BRAHIMI <brahimi.sdhmd@gmail.com>
2025-02-03 00:31:48 +0100

    more cleanup bfs version of same tree

--- core/bfs/same_tree.py
+++ core/bfs/same_tree.py
@@ -1,29 +1,23 @@
-# 100. Same Tree https://leetcode.com/problems/same-tree/description/?envType=problem-list-v2&envId=breadth-first-search
-
-from typing import Optional
-from queue import Queue
-
 # Definition for a binary tree node.
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+from typing import Optional
+from queue import Queue
 
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        q1, q2 = Queue(), Queue()
+        queue = Queue()
         if not p and not q:
             return True
         if ((not p) ^ (not q)):
             return False
-        q1.put(p)
-        q2.put(q)
-        while q1.empty() == False and q2.empty() == False:
-            node1 = q1.get()
-            node2 = q2.get()
+        queue.put((p, q))
+        while queue.empty() == False:
+            node1, node2 = queue.get()
 
             if node1.val != node2.val:
                 return False
@@ -33,10 +27,10 @@ class Solution:
                 return False
 
             if node1.left:
-                q1.put(node1.left)
-                q2.put(node2.left)
+                queue.put((node1.left, node2.left))
             if node1.right:
-                q1.put(node1.right)
-                q2.put(node2.right)
+                queue.put((node1.right, node2.right))
 
         return True
+
+        
